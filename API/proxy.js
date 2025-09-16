81c254a464a83ba09068e45d4f9b749b02a3e67d
Hacker1000000000 <95496750+Hacker1000000@users.noreply.github.com>
2025-09-16 13:03:24 -0500

    Update proxy.js

--- API/proxy.js
+++ API/proxy.js
@@ -1,8 +1,6 @@
 const Unblocker = require('unblocker');
-
 const unblocker = new Unblocker();
 
 module.exports = (req, res) => {
-  // Adapt Express handler to Vercel serverless
   unblocker(req, res);
 };
